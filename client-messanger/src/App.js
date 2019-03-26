839c332bf6b69b09acc88cb29215442783d77364
siratl <elishubax@gmail.com>
2019-03-25 23:40:51 -0700

    Created functioning app that sends sms to phone number using twilio

--- client-messanger/src/App.js
+++ client-messanger/src/App.js
@@ -1,11 +1,65 @@
 import React, { Component } from 'react';
 import './App.css';
+import { Button, Form, FormGroup, Label, Input, Col } from 'reactstrap';
 
 class App extends Component {
+  state = {
+    sms: {
+      messageText: '',
+    },
+
+    error: false,
+  };
+
+  sendSMS = ev => {
+    ev.preventDefault();
+    const { sms } = this.state;
+
+    if (this.state.sms.messageText === '') {
+      return this.setState({ error: true });
+    } else {
+      fetch(
+        `http://localhost:4444/api/send-sms?messageText=${sms.messageText}`,
+      );
+      this.setState({ sms: { messageText: '' }, error: false });
+    }
+  };
+
+  handleInputChange = ev => {
+    this.setState({ sms: { [ev.target.name]: ev.target.value }, error: false });
+  };
+
   render() {
     return (
       <div className="App">
-        <header className="App-header" />
+        <header className="App-header">
+          <h1>Send SMS</h1>
+        </header>
+
+        <main className="App-body">
+          <Form onSubmit={this.sendSMS} className="App-form">
+            <FormGroup row>
+              <Label for="messageText" sm={2}>
+                Message
+              </Label>
+              <Col sm={10}>
+                <Input
+                  type="textarea"
+                  name="messageText"
+                  onChange={this.handleInputChange}
+                  placeholder={
+                    this.state.error
+                      ? 'THIS FIELD CANNOT BE EMPTY!'
+                      : 'Type your message here...'
+                  }
+                  value={this.state.sms.messageText}
+                  id={this.state.error ? 'messageTextError' : 'messageText'}
+                />
+              </Col>
+            </FormGroup>
+            <Button type="submit">Send Message</Button>
+          </Form>
+        </main>
       </div>
     );
   }
--- server/api/server.js
+++ server/api/server.js
@@ -13,8 +13,28 @@ const server = express();
 server.use(cors());
 server.use(express.json());
 
+//Server Home Page
 server.get('/', (req, res) => {
   res.send('Express Server Running...');
+});
+
+//Twilio Server route
+server.get('/api/send-sms', (req, res) => {
+  const { recipient = process.env.MY_NUMBER, messageText } = req.query;
+  console.log(req.query);
+
+  client.messages
+    .create({
+      from: '+19093031203',
+      to: recipient,
+      body: messageText,
+    })
+    .then(response => {
+      console.log(response);
+    })
+    .catch(err => {
+      res.json(err);
+    });
 });
 
 module.exports = server;
